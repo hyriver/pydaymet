694a61a23a864ef28262f06445c801d2429ff664
cheginit <cheginit@gmail.com>
2022-06-14 10:47:25 -0500

    ENH: Remove the unnecessary check for importlib.metadata since the min python version is now 3.8 [skip ci]

--- pydaymet/__init__.py
+++ pydaymet/__init__.py
@@ -1,4 +1,6 @@
 """Top-level package for PyDaymet."""
+import importlib.metadata
+
 from .core import Daymet
 from .exceptions import (
     InvalidInputRange,
@@ -11,14 +13,7 @@ from .pet import potential_et
 from .print_versions import show_versions
 from .pydaymet import get_bycoords, get_bygeom
 
-try:
-    import importlib.metadata
-except ImportError:
-    import importlib_metadata
-
-    __version__ = importlib_metadata.version("pydaymet")
-else:
-    __version__ = importlib.metadata.version("pydaymet")
+__version__ = importlib.metadata.version("pydaymet")
 
 __all__ = [
     "Daymet",
