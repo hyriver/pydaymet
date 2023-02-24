5a4b3d8624553903e4e1d23f01978c0cb88a1ebb
Taher Chegini <cheginit@gmail.com>
2023-02-24 00:29:34 -0600

    MNT: Remove disabling pygeos flag, since it is not a dependecy anymore. [skip ci]

--- pydaymet/__init__.py
+++ pydaymet/__init__.py
@@ -1,11 +1,6 @@
 """Top-level package for PyDaymet."""
 from importlib.metadata import PackageNotFoundError, version
 
-if int(version("shapely").split(".")[0]) > 1:
-    import os
-
-    os.environ["USE_PYGEOS"] = "0"
-
 from pydaymet.core import Daymet
 from pydaymet.exceptions import (
     InputRangeError,
