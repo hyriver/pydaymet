90da5c1ea4dfec9269b792b5befea37eb7e40b00
Taher Chegini <cheginit@gmail.com>
2025-01-17 12:54:19 -0500

    MNT: Add aiofiles and aiohttp as deps for downloading requests and remove dep on async-retriever. [skip ci]

---
+++ src/pydaymet/_streaming.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import asyncio
+import contextlib
+import sys
+from typing import TYPE_CHECKING
+
+import aiofiles
+from aiohttp import ClientSession, TCPConnector, ClientTimeout
+from aiohttp.client_exceptions import ClientResponseError
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from pathlib import Path
+
+__all__ = ["stream_write"]
+CHUNK_SIZE = 1024 * 1024  # Default chunk size of 1 MB
+MAX_HOSTS = 5  # Maximum connections to a single host (rate-limited service)
+TIMEOUT = 10 * 60  # Timeout for requests in seconds
+
+if sys.platform == "win32":  # pragma: no cover
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+class ServiceError(Exception):
+    """Exception raised for download errors."""
+
+    def __init__(self, err: str, url: str | None = None) -> None:
+        self.message = (
+            f"Service returned the following error:\nURL: {url}\nERROR: {err}" if url else err
+        )
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+async def _stream_file(session: ClientSession, url: str, filepath: Path) -> None:
+    """Stream the response to a file, skipping if already downloaded."""
+    try:
+        async with session.get(url) as response:
+            remote_size = int(response.headers.get("Content-Length", -1))
+            if filepath.exists() and filepath.stat().st_size == remote_size:
+                return
+
+            async with aiofiles.open(filepath, "wb") as file:
+                async for chunk in response.content.iter_chunked(CHUNK_SIZE):
+                    await file.write(chunk)
+    except (ClientResponseError, ValueError) as ex:
+        filepath.unlink(missing_ok=True)
+        raise ServiceError(await response.text(), str(response.url)) from ex
+
+
+async def _stream_session(urls: Sequence[str], files: Sequence[Path]) -> None:
+    """Download files concurrently."""
+    async with ClientSession(
+        connector=TCPConnector(limit_per_host=MAX_HOSTS),
+        raise_for_status=True,
+        timeout=ClientTimeout(TIMEOUT),
+    ) as session:
+        tasks = [_stream_file(session, url, filepath) for url, filepath in zip(urls, files)]
+        await asyncio.gather(*tasks)
+
+
+def _get_or_create_event_loop() -> tuple[asyncio.AbstractEventLoop, bool]:
+    """Retrieve or create an event loop."""
+    with contextlib.suppress(RuntimeError):
+        return asyncio.get_running_loop(), False
+    new_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(new_loop)
+    return new_loop, True
+
+
+def stream_write(urls: Sequence[str], file_paths: Sequence[Path]) -> None:
+    """Download multiple files concurrently by streaming their content to disk."""
+    parent_dirs = {filepath.parent for filepath in file_paths}
+    for parent_dir in parent_dirs:
+        parent_dir.mkdir(parents=True, exist_ok=True)
+
+    loop, is_new_loop = _get_or_create_event_loop()
+
+    try:
+        loop.run_until_complete(_stream_session(urls, file_paths))
+    finally:
+        if is_new_loop:
+            loop.close()
